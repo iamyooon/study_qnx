58717d769b13f66e4d605294618f12efb3a704f8
seokhoon.yoon <seokhoon.yoon@lgepartner.com>
2021-06-21 19:02:06 +0900

    tmp

--- language/shell/get_period_from_timestamp_column.sh
+++ language/shell/get_period_from_timestamp_column.sh
@@ -5,24 +5,26 @@ turn_on_time=0
 turn_off_time=0
 period_time=0
 
-a="10"
-b="5"
-echo "$a-$b" | bc
-
 cat sample_dlt.log |
 while IFS= read -r line; do
 	if echo $line | grep -q "\- data :"; then
                  if echo $line | grep -q "DID\"\":55"; then
 			# get timestamp
 			 turn_on_signal=1
-			 turn_on_time=`echo $line | cut -d',' -f2`
+
+			# error occured below
+			#
+			# (standard_in) 1: syntax error
+			#
+			#turn_on_time=`echo $line | cut -d',' -f2`
+			 turn_on_time=`echo $line | cut -d',' -f2 | tr -d '"'`
                          echo "turn on=$turn_on_time"
                  else
 			# get timestamp and calculate period(CURR - PREV)
-			 turn_off_time=`echo $line | cut -d',' -f2`
+			 turn_off_time=`echo $line | cut -d',' -f2 | tr -d '"'`
                          echo "turn off=$turn_off_time"
 			 if [ "$turn_on_signal" == 1 ]; then
-				 echo "$turn_off_time - $turn_on_time" | bc
+				 period_time=`echo "$turn_off_time - $turn_on_time" | bc`
 				 echo "period=$period_time,$turn_off_time,$turn_on_time"
 				 turn_on_signal=0
 			 fi
